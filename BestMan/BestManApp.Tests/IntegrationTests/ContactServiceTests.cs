96ea5bf7f2032cc685f1ee5ec1c2ee4f2d77c0db
Ariel Ben Horesh <ariel_bh@yahoo.com>
2013-02-25 02:51:59 +0200

    Part 7: Adding Unitest project

---
+++ BestMan/BestManApp.Tests/IntegrationTests/ContactServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using BestManApp.Services;
+using Microsoft.Phone.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BestManApp.Tests.IntegrationTests
+{
+    [TestClass]
+    public class ContactServiceTests : WorkItemTest
+    {
+        [TestMethod]
+        public void TestDemo()
+        {
+            int a = 1;
+            int b = 2;
+            Assert.AreEqual(3, a + b);
+        }
+
+        [TestMethod]
+        [Asynchronous]
+        public async Task GetAllContacts_ReturnsContactsFromEmulator()
+        {
+            ContactService contactService = new ContactService();
+            var result = await contactService.GetAllContacts();
+            Assert.IsTrue(result.Length > 0);
+            EnqueueTestComplete();   
+        }
+         
+    }
+}
